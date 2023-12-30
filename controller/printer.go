eed3da204ce852941f36738440ee137b7a02e647
Rittiphon Phoarun <miw1513@hotmail.com>
2023-12-31 04:22:23 +0700

    update print slip

--- controller/printer.go
+++ controller/printer.go
@@ -11,235 +11,238 @@ import (
 	"net"
 	"strings"
 
-	"github.com/gin-gonic/gin"
 	"github.com/kenshaw/escpos"
 )
 
-func Printer() func(c *gin.Context) {
-	type Body struct {
-		IP   string     `form:"ip" json:"ip" binding:"required"`
-		Bill model.Bill `bson:"bill_data" form:"bill_data" json:"bill_data" binding:"required"`
+func Printer(IP string, Bill model.Bill) { //func(c *gin.Context) {
+	// type Body struct {
+	// 	IP   string     `form:"ip" json:"ip" binding:"required"`
+	// 	Bill model.Bill `bson:"bill_data" form:"bill_data" json:"bill_data" binding:"required"`
+	// }
+	// return func(c *gin.Context) {
+	// var Body Body
+	// if err := c.ShouldBind(&Body); err != nil {
+	// 	c.JSON(400, gin.H{
+	// 		"code":  400,
+	// 		"msg":   "ERROR BINDING",
+	// 		"error": err.Error(),
+	// 	})
+	// 	return
+	// }
+	printerIP := IP
+	printerPort := 9100
+
+	// Create a connection to the network printer
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", printerIP, printerPort))
+	if err != nil {
+		// c.JSON(400, gin.H{
+		// 	"code":  400,
+		// 	"msg":   "Error Printer Connection",
+		// 	"error": err.Error(),
+		// })
+		fmt.Println("Error Printer Connection")
+
+		return
 	}
-	return func(c *gin.Context) {
-		var Body Body
-		if err := c.ShouldBind(&Body); err != nil {
-			c.JSON(400, gin.H{
-				"code":  400,
-				"msg":   "ERROR BINDING",
-				"error": err.Error(),
-			})
-			return
-		}
-		printerIP := Body.IP
-		printerPort := 9100
 
-		// Create a connection to the network printer
-		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", printerIP, printerPort))
-		if err != nil {
-			c.JSON(400, gin.H{
-				"code":  400,
-				"msg":   "Error Printer Connection",
-				"error": err.Error(),
-			})
+	defer conn.Close()
 
-			return
-		}
+	// Create a new ESC/POS printer
+	printer := escpos.New(conn)
 
-		defer conn.Close()
+	// Initialize the printer
 
-		// Create a new ESC/POS printer
-		printer := escpos.New(conn)
+	// Load your JSON data here (data from test.json)
+	data := Bill
+	// return
+	orders := data.Order
+	item := 0
 
-		// Initialize the printer
+	// Initialize the formatter for currency
+	// formatter := NewCurrencyFormatter("th-TH", "THB")
 
-		// Load your JSON data here (data from test.json)
-		data := Body.Bill
-		// return
-		orders := data.Order
-		item := 0
+	// Get the current date and time
+	dataTime := data.Datetime
+	dateTime := dataTime.Format("02/01/2006 15:04")
 
-		// Initialize the formatter for currency
-		// formatter := NewCurrencyFormatter("th-TH", "THB")
+	fmt.Println(dateTime)
 
-		// Get the current date and time
-		dataTime := data.Datetime
-		dateTime := dataTime.Format("02/01/2006 15:04")
+	// Print the receipt
+	printer.Init()
+	// if err != nil {
+	// 	c.JSON(400, gin.H{
+	// 		"code":  400,
+	// 		"msg":   "ERROR CALL API DOMAIN",
+	// 		"error": err.Error(),
+	// 	})
 
-		fmt.Println(dateTime)
+	// 	return
+	// }
+	defer printer.End()
+	defer printer.Cut()
 
-		// Print the receipt
-		printer.Init()
-		// if err != nil {
-		// 	c.JSON(400, gin.H{
-		// 		"code":  400,
-		// 		"msg":   "ERROR CALL API DOMAIN",
-		// 		"error": err.Error(),
-		// 	})
+	// printer.SetSmooth(128)
+	printer.SetLang("en")
+	printer.SetFont("B")
+	printer.SetAlign("center")
 
-		// 	return
-		// }
-		defer printer.End()
-		defer printer.Cut()
+	printer.SetFontSize(3, 2)
 
-		// printer.SetSmooth(128)
-		printer.SetLang("en")
-		printer.SetFont("B")
-		printer.SetAlign("center")
+	// Print the header
+	printer.SetEmphasize(1)
+	printer.Write("MAX WALLET\n\n")
 
-		printer.SetFontSize(3, 2)
+	printer.SetFontSize(1, 1)
+	printer.SetFont("A")
+	printer.Write("Receipt\n")
+	printer.SetAlign("center")
+	printer.Write("----------------------------------------------\n")
 
-		// Print the header
-		printer.SetEmphasize(1)
-		printer.Write("MAX WALLET\n\n")
+	// // Print table, cashier, customer, and date/time
+	printer.SetAlign("left")
+	printer.Write(fmt.Sprintf("TABLE: %s\n", data.Table))
+	printer.Write(fmt.Sprintf("CASHIER: %s\n", data.Operator))
 
-		printer.SetFontSize(1, 1)
-		printer.SetFont("A")
-		printer.Write("Receipt\n")
-		printer.SetAlign("center")
-		printer.Write("----------------------------------------------\n")
+	var CustomerName string
+	if len(data.Customer.Aka) == 0 {
+		CustomerName = data.Customer.MemberID
+	} else {
+		CustomerName = data.Customer.Aka
+	}
 
-		// // Print table, cashier, customer, and date/time
-		printer.SetAlign("left")
-		printer.Write(fmt.Sprintf("TABLE: %s\n", data.Table))
-		printer.Write(fmt.Sprintf("CASHIER: %s\n", data.Operator))
+	printer.Write(fmt.Sprintf("CUSTOMER: %s\n", CustomerName))
 
-		var CustomerName string
-		if len(data.Customer.Aka) == 0 {
-			CustomerName = data.Customer.MemberID
+	dateLine := fmt.Sprintf("DATE: %s", dataTime.Format("02/01/2006"))
+	timeLine := fmt.Sprintf("TIME: %s", dataTime.Format("15:04"))
+	totalLine := len(dateLine) + len(fmt.Sprint(timeLine))
+	spacesNeeded := service.Max(0, 48-totalLine)
+	spaces := service.Repeat(" ", spacesNeeded)
+	dataText := dateLine + spaces + timeLine
+
+	printer.SetAlign("left")
+	printer.Write(dataText + "\n")
+	printer.SetAlign("center")
+	printer.Write("----------------------------------------------\n")
+
+	// Print order details
+	for _, order := range orders {
+
+		var moreText string
+		var defaultText string
+		var orderLine string
+		var PercentDecision float64 = 0.70
+		var countTextOder int
+		if len(order.Name) >= 20 {
+			splitText := strings.Split(order.Name, " ")
+
+			for i, v := range splitText {
+				if countTextOder >= 24 {
+					PercentDecision = 0.10
+				}
+				percentage := float64(len(splitText)) * PercentDecision
+				count := int(percentage)
+
+				countTextOder += len(v)
+
+				if len(splitText) <= 2 && countTextOder >= 24 {
+					totalCharacter := countTextOder - len(v)
+					for _, eachCharacter := range v {
+						characterAsString := string(eachCharacter)
+
+						if totalCharacter <= 24 {
+							totalCharacter += 1
+							defaultText += characterAsString
+						} else {
+							moreText += characterAsString
+						}
+					}
+				} else {
+					if i >= count {
+						moreText += v + " "
+					} else {
+						defaultText += v + " "
+					}
+				}
+
+				orderLine = fmt.Sprintf("%d   %s ", order.Quantity, defaultText)
+			}
 		} else {
-			CustomerName = data.Customer.Aka
+			orderLine = fmt.Sprintf("%d   %s", order.Quantity, order.Name)
 		}
 
-		printer.Write(fmt.Sprintf("CUSTOMER: %s\n", CustomerName))
-
-		dateLine := fmt.Sprintf("DATE: %s", dataTime.Format("02/01/2006"))
-		timeLine := fmt.Sprintf("TIME: %s", dataTime.Format("15:04"))
-		totalLine := len(dateLine) + len(fmt.Sprint(timeLine))
+		// price := formatter.Format(order.Price)
+		price := service.FormatCurrency(order.Price)
+		totalLine := len(orderLine) + len(fmt.Sprint(price))
 		spacesNeeded := service.Max(0, 48-totalLine)
 		spaces := service.Repeat(" ", spacesNeeded)
-		dataText := dateLine + spaces + timeLine
+		orderLineData := orderLine + spaces + price
+		orderLineData = service.ReplaceThbSymbol(orderLineData)
+		fmt.Println(orderLineData)
 
-		printer.SetAlign("left")
-		printer.Write(dataText + "\n")
-		printer.SetAlign("center")
-		printer.Write("----------------------------------------------\n")
-
-		// Print order details
-		for _, order := range orders {
-
-			var moreText string
-			var defaultText string
-			var orderLine string
-			var PercentDecision float64 = 0.70
-			var countTextOder int
-			if len(order.Name) >= 20 {
-				splitText := strings.Split(order.Name, " ")
-
-				for i, v := range splitText {
-					if countTextOder >= 24 {
-						PercentDecision = 0.10
-					}
-					percentage := float64(len(splitText)) * PercentDecision
-					count := int(percentage)
-
-					countTextOder += len(v)
-
-					if len(splitText) <= 2 && countTextOder >= 24 {
-						totalCharacter := countTextOder - len(v)
-						for _, eachCharacter := range v {
-							characterAsString := string(eachCharacter)
-
-							if totalCharacter <= 24 {
-								totalCharacter += 1
-								defaultText += characterAsString
-							} else {
-								moreText += characterAsString
-							}
-						}
-					} else {
-						if i >= count {
-							moreText += v + " "
-						} else {
-							defaultText += v + " "
-						}
-					}
-
-					orderLine = fmt.Sprintf("%d   %s ", order.Quantity, defaultText)
-				}
-			} else {
-				orderLine = fmt.Sprintf("%d   %s", order.Quantity, order.Name)
-			}
-
-			// price := formatter.Format(order.Price)
-			price := service.FormatCurrency(order.Price)
-			totalLine := len(orderLine) + len(fmt.Sprint(price))
-			spacesNeeded := service.Max(0, 48-totalLine)
-			spaces := service.Repeat(" ", spacesNeeded)
-			orderLineData := orderLine + spaces + price
-			orderLineData = service.ReplaceThbSymbol(orderLineData)
-			fmt.Println(orderLineData)
-
-			if len(order.Name) >= 20 {
-				fmt.Println()
-				printer.Write(orderLineData + "\n")
-				moreSpace := service.Max(0, 48-len(moreText))
-				spaces := service.Repeat(" ", moreSpace)
-				MoreOrderLineData := service.ReplaceThbSymbol(fmt.Sprintf("    %s", moreText) + spaces)
-				printer.Write(MoreOrderLineData + "\n")
-			} else {
-				printer.Write(orderLineData + "\n\n")
-			}
-			item += order.Quantity
+		if len(order.Name) >= 20 {
+			fmt.Println()
+			printer.Write(orderLineData + "\n")
+			moreSpace := service.Max(0, 48-len(moreText))
+			spaces := service.Repeat(" ", moreSpace)
+			MoreOrderLineData := service.ReplaceThbSymbol(fmt.Sprintf("    %s", moreText) + spaces)
+			printer.Write(MoreOrderLineData + "\n")
+		} else {
+			printer.Write(orderLineData + "\n\n")
 		}
-
-		// // Calculate and print the summary
-		printer.SetAlign("center")
-		printer.Write("----------------------------------------------\n")
-		printer.SetAlign("left")
-		printer.Write(fmt.Sprintf("ITEMS: %d\n", item))
-
-		grandTotal := data.PriceWithDiscount + data.ServiceCharge
-
-		maxLengthAmount := 0
-		for _, eachAmount := range []float64{data.Price, data.Discount, data.ServiceCharge, grandTotal, data.Vat, data.Rounding} {
-			stringAmount := service.FormatCurrency(eachAmount)
-			if len(stringAmount) > maxLengthAmount {
-
-				maxLengthAmount = len(stringAmount)
-				fmt.Println(stringAmount, maxLengthAmount)
-			}
-		}
-
-		fmt.Println("maxLengthAmount", maxLengthAmount)
-		// var maxLengthPriceEndSlip int
-
-		// // Print subtotal, discount, service charge, and other details
-		service.PrintTextWithSpace(printer, "Subtotal: ", data.Price, 35, maxLengthAmount)
-		service.PrintTextWithSpace(printer, "Discount("+fmt.Sprint(data.DiscountPercent)+"%): ", data.Discount, 35, maxLengthAmount)
-		service.PrintTextWithSpace(printer, fmt.Sprintf("Service Charge(%d%%): ", data.PercentServiceCharge), (data.ServiceCharge), 35, maxLengthAmount)
-
-		service.PrintTextWithSpace(printer, "Before VAT: ", grandTotal, 35, maxLengthAmount)
-		service.PrintTextWithSpace(printer, "VAT(7%): ", data.Vat, 35, maxLengthAmount)
-		service.PrintTextWithSpace(printer, "Rounding: ", data.Rounding, 35, maxLengthAmount)
-
-		printer.SetAlign("right")
-		printer.Write("=========================\n")
-		service.PrintTextWithSpace(printer, "Total: ", (data.Total), 24, 0)
-		printer.SetAlign("right")
-		printer.Write("=========================\n")
-
-		printer.SetAlign("center")
-		printer.Write("----------------------------------------------\n")
-		printer.SetAlign("center")
-		printer.Write("Thank you for your order!\n")
-
-		c.JSON(200, gin.H{
-			"code": 200,
-			"msg":  "Print Successful",
-		})
-
+		item += order.Quantity
 	}
+
+	// // Calculate and print the summary
+	printer.SetAlign("center")
+	printer.Write("----------------------------------------------\n")
+	printer.SetAlign("left")
+	printer.Write(fmt.Sprintf("ITEMS: %d\n", item))
+
+	grandTotal := data.PriceWithDiscount + data.ServiceCharge
+
+	maxLengthAmount := 0
+	for _, eachAmount := range []float64{data.Price, data.Discount, data.ServiceCharge, grandTotal, data.Vat, data.Rounding} {
+		stringAmount := service.FormatCurrency(eachAmount)
+		if len(stringAmount) > maxLengthAmount {
+
+			maxLengthAmount = len(stringAmount)
+			fmt.Println(stringAmount, maxLengthAmount)
+		}
+	}
+
+	fmt.Println("maxLengthAmount", maxLengthAmount)
+	// var maxLengthPriceEndSlip int
+
+	// // Print subtotal, discount, service charge, and other details
+	service.PrintTextWithSpace(printer, "Subtotal: ", data.Price, 35, maxLengthAmount)
+	service.PrintTextWithSpace(printer, "Discount("+fmt.Sprint(data.DiscountPercent)+"%): ", data.Discount, 35, maxLengthAmount)
+	service.PrintTextWithSpace(printer, fmt.Sprintf("Service Charge(%d%%): ", data.PercentServiceCharge), (data.ServiceCharge), 35, maxLengthAmount)
+
+	service.PrintTextWithSpace(printer, "Before VAT: ", grandTotal, 35, maxLengthAmount)
+	service.PrintTextWithSpace(printer, "VAT(7%): ", data.Vat, 35, maxLengthAmount)
+	service.PrintTextWithSpace(printer, "Rounding: ", data.Rounding, 35, maxLengthAmount)
+
+	printer.SetAlign("right")
+	printer.Write("=========================\n")
+	service.PrintTextWithSpace(printer, "Total: ", (data.Total), 24, 0)
+	printer.SetAlign("right")
+	printer.Write("=========================\n")
+
+	printer.SetAlign("center")
+	printer.Write("----------------------------------------------\n")
+	printer.SetAlign("center")
+	printer.Write("Thank you for your order!\n")
+
+	// c.JSON(200, gin.H{
+	// 	"code": 200,
+	// 	"msg":  "Print Successful",
+	// })
+
+	fmt.Println("Print Success")
+	return
 }
+
+// }
 
 func loadData() model.Bill {
 	// Read the JSON file
--- server.go
+++ server.go
@@ -1,44 +1,143 @@
 package escpos
 
 import (
+	"context"
+	"encoding/json"
 	"escpos/controller"
-	"escpos/service"
+	"escpos/model"
 	"fmt"
-	"net/http"
+	"log"
+	"time"
 
+	firebase "firebase.google.com/go/v4"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
 )
 
 func StartServer() {
 	// controller.Printer()
 
-	// Start Gin GO
-	router := gin.Default()
-	router.Use(gin.Recovery())
-	router.Use(CORS)
+	// // Start Gin GO
+	// router := gin.Default()
+	// router.Use(gin.Recovery())
+	// // router.Use(CORS)
 
-	routerGroup := router.Group("/api")
+	// routerGroup := router.Group("/api")
 
-	NewRoutes(routerGroup)
-	router.Run(":8000")
+	// NewRoutes(routerGroup)
+	// router.Run(":8000")
+	fmt.Println("START Bill Application")
+	app, err := initFirebaseWithStringCredentials()
+	if err != nil {
+		fmt.Println("ERROR Init Firebase")
+		return
+	}
+
+	client, err := app.Database(context.Background())
+	if err != nil {
+		// Handle error
+		fmt.Println("ERROR CONNECT", err)
+		return
+	}
+	refAllBill := client.NewRef("bills/")
+	// Reference the desired data location
+
+	// read from user_scores using ref
+	// type UserScore struct {
+	// 	Score int `json:"score"`
+	// }
+	// for  {
+
+	// }
+	for {
+
+		fmt.Println("Start Get Bills")
+		var bills map[string]interface{}
+
+		if err := refAllBill.Get(context.TODO(), &bills); err != nil {
+			log.Fatalln("error in reading from firebase DB: ", err)
+			continue
+		}
+		fmt.Println("Total Bills", len(bills))
+		for i, v := range bills {
+			fmt.Println("Key", i)
+			var bill model.Bill
+			billbytes, _ := json.Marshal(v)
+			json.Unmarshal(billbytes, &bill)
+
+			controller.Printer("192.168.1.148", bill)
+			refEachBill := client.NewRef("bills/" + i + "/")
+
+			if err := refEachBill.Delete(context.TODO()); err != nil {
+				log.Fatalln("error in deleting ref: ", err)
+				continue
+			}
+			fmt.Println("deleted successfully:)")
+			time.Sleep(500 * time.Millisecond)
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	// ref := client.NewRef("user_scores/1")
+
+	// Listen for data changes
+	// listener := ref.Listen(context.Background())
+	// defer listener.Close() // Close the listener when finished
+
+	// go func() {
+	// 	for {
+	// 		snap, err := listener.Next()
+	// 		if err != nil {
+	// 			// Handle error
+	// 		}
+
+	// 		// Create a worker process for each new data change
+	// 		go func() {
+	// 			// Process the new data within this worker process
+	// 			fmt.Println("New data:", snap.Val())
+	// 			// Perform the necessary actions based on the data
+	// 		}()
+	// 	}
+	// }()
+}
+
+func initFirebaseWithStringCredentials() (*firebase.App, error) {
+	credentials := map[string]interface{}{
+		"type":                        "service_account",
+		"project_id":                  "maxwallet",
+		"private_key_id":              "ac44791f21c56a053423c47490224c79226f2793",
+		"private_key":                 "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC+H7Igend6GcBD\nT1BSILnSmP1e53so6XZTLw0k+m9605DFDGX07Ez73PTLJgqR5CUDLK9oszasW2h3\nnhkqlLFw82wHFVmJ8HlkarxDQNLkwgqUBL6T6IxaHDmVrjUZTCY0QXLsjGBEwBgG\nwIFjhO2x95EYQNUVhcdjqLfEioigiYE9VTL4rQX5QFSWOTelTTHAW30fDlJzgrpy\npXNqX0bs7F/4sNMOqC78KpozwCVnHxQ5DTt3MPibNNsu7+wEGx+Pi0K3zr20O+Zd\nVcyUs0hlk+1Ga/ErUu+A5oJyAi800EqrVNUkxJFp+5xxpAYQSjInF1oKZgCAKo5D\nND+yEHKXAgMBAAECggEAMrH66kQdhZZGOOx2K2AEjI40yVCJ+4+8+FNMeva4MRUm\nvhQfP56erG3vbBfZq6sc32rT3uQiiPTat0KVzU/WvJp7zKu5s1lG1SrMxlYLpenD\nrlJRitjwS7rI9At+px8x9K8a+M45gTuDbJF6LtIsG087Bi+LYfDJCN7dGXAjHEL3\nbORbkWTilIDgqThdK7JtDzDvTUUkzghzzncM6AcyEx2wFbLiLy+6FZjJvaobTk93\nDwChSK7jM0Rj/e1gPGjgRe/mZh/3qDIN3mborO8tRcZ31yMbEBAt+OeRDfm8oLDB\nsJXU4HlpynLcmv2UmtHQbpzgvjI5LW9uMZOgRjIqcQKBgQD67ShpVRq/3kc2DPVo\n5OR9Jma5xJFv+fXeEegSj3nKLhfmdotRBW57zfWLrL9la/D1udIZCmWQVAgkD/xC\nldJBCfUMD0KKK4zP4IX7okEiv53Y7kIt25JBREEVMvdWRvkf0XAS39huqNLD2Nqc\nrCUTfUsvNOwmWWzZ3QQxYDqW7wKBgQDB98/2MRQzXOsgaO7dNbLk5EmqFIAqQTgZ\n6DDtmzasXcuQlyAEH2BxokbwV77x3wq0tMPLowKFPOHPAnQA8zZqXmHWLSoJx8xX\nig7lU6mBJlavFAOzWxK7YFMRXw35OZZhuSEyKFoEjJ3skPYRvMSxbitmBEvY5X92\n4sULJLGe2QKBgDtNy6xBWeYY4ZmrrGCTIFFXvxWOmJTvbaWDc+bXFACtriZgxAJt\nFzSZc7wEIuQUg8l/lmEmrORUh+wF/ye5gwyuDsU/4gkHy+rhdKkJKv3MbcD0Zp2x\n9DoKqJsbBYvVkFFtzWAYmW1l5xI0cU5v/P9DMH7CskFKB0jiTHhi9tXvAoGBAME2\nmt8/8EFhs41rOVT84qCCjqZvGWP952ZXFjX5QLLeE6KKB4hTwPwwi71pinjglod+\n8PJuBFq4VK6iYO95VaELNyXjg1aOwYwJp+DkP5q4l+x6YV1NwREJWHWbXA4AQT5C\n7UBLVa/maoF1vMfaY4vilDRg3zTFFv1T0rfQ62WxAoGAa4UK2NaX1qJzTOb0+IdM\nlnpDOtI5tmgxsS8X/Uz/AxzRjfdmDSmNUI5IM+AYphv1Jy4vdGHk3ST3OiKJAL8x\nq8nq+GLVRhvgblSQuBWja+v7to/W5drG7ndaYVyniX9W0BvC8NgBEHHxomlfnZR6\nZxYqpOHrQJQcysG1DN86ToQ=\n-----END PRIVATE KEY-----\n",
+		"client_email":                "firebase-adminsdk-l1ved@maxwallet.iam.gserviceaccount.com",
+		"client_id":                   "108462313032826368896",
+		"auth_uri":                    "https://accounts.google.com/o/oauth2/auth",
+		"token_uri":                   "https://oauth2.googleapis.com/token",
+		"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+		"client_x509_cert_url":        "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-l1ved%40maxwallet.iam.gserviceaccount.com",
+		"universe_domain":             "googleapis.com",
+	}
+
+	credentialsString, err := json.Marshal(credentials)
+	if err != nil {
+		fmt.Println("Error marshaling credentials:", err)
+		return nil, err
+	}
+	conf := &firebase.Config{
+		DatabaseURL: "https://maxwallet-default-rtdb.asia-southeast1.firebasedatabase.app",
+	}
+
+	opt := option.WithCredentialsJSON(credentialsString)
+	return firebase.NewApp(context.Background(), conf, opt)
 }
 
 func NewRoutes(app *gin.RouterGroup) {
 
 	app.GET("/", getAPI())
-	app.POST("/print", controller.Printer())
+	// app.POST("/print", controller.Printer())
 
 }
 
 func getAPI() func(c *gin.Context) {
 	return func(c *gin.Context) {
-		service.PrintTextWithSpaceTest(nil, "Subtotal: ", 412880.00, 35)
-		service.PrintTextWithSpaceTest(nil, "Discount: ", 0.00, 35)
-		service.PrintTextWithSpaceTest(nil, fmt.Sprintf("Service Charge(%d%%): ", 10), (41288), 35)
-
-		service.PrintTextWithSpaceTest(nil, "Before VAT: ", 412880+41288, 35)
-		service.PrintTextWithSpaceTest(nil, "VAT(7%): ", 31791.760000000002, 35)
-		service.PrintTextWithSpaceTest(nil, "Rounding: ", 0.23999999999068677, 35)
 
 		c.JSON(200, gin.H{
 			"message": "API-ESCPOS",
@@ -46,28 +145,28 @@ func getAPI() func(c *gin.Context) {
 	}
 }
 
-func CORS(c *gin.Context) {
-	// First, we add the headers with need to enable CORS
-	// Make sure to adjust these headers to your needs
-	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Access-Control-Allow-Methods", "*")
-	c.Header("Access-Control-Allow-Headers", "*")
-	c.Header("Content-Type", "application/json")
+// func CORS(c *gin.Context) {
+// 	// First, we add the headers with need to enable CORS
+// 	// Make sure to adjust these headers to your needs
+// 	c.Header("Access-Control-Allow-Origin", "*")
+// 	c.Header("Access-Control-Allow-Methods", "*")
+// 	c.Header("Access-Control-Allow-Headers", "*")
+// 	c.Header("Content-Type", "application/json")
 
-	// Second, we handle the OPTIONS problem
-	if c.Request.Method != "OPTIONS" {
-		c.Next()
-		return
-	} else {
-		// Everytime we receive an OPTIONS request,
-		// we just return an HTTP 200 Status Code
-		// Like this, Angular can now do the real
-		// request using any other method than OPTIONS
-		// c.AbortWithStatusJSON(401, gin.H{
-		// 	"code":    "ERROR",
-		// 	"message": "Unauthorized",
-		// })
-		c.AbortWithStatus(http.StatusOK)
-		return
-	}
-}
+// 	// Second, we handle the OPTIONS problem
+// 	if c.Request.Method != "OPTIONS" {
+// 		c.Next()
+// 		return
+// 	} else {
+// 		// Everytime we receive an OPTIONS request,
+// 		// we just return an HTTP 200 Status Code
+// 		// Like this, Angular can now do the real
+// 		// request using any other method than OPTIONS
+// 		// c.AbortWithStatusJSON(401, gin.H{
+// 		// 	"code":    "ERROR",
+// 		// 	"message": "Unauthorized",
+// 		// })
+// 		c.AbortWithStatus(http.StatusOK)
+// 		return
+// 	}
+// }
